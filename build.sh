00aa2880e4fba4f2a33416563e7e4c4c32a7fcc4
Atharva Nandanwar <Atharva.Nandanwar@Colorado.EDU>
2020-04-10 19:57:25 -0600

    updated to script to add meta-aesd-project layer

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@
 # Purpose - Used to flash code into STM32MP1 connected through usb
 
 DISTRO=openstlinux-weston MACHINE=stm32mp1 source ./layers/meta-st/scripts/envsetup.sh
+bitbake-layers add-layer ../layers/meta-st/meta-aesd-project
 bitbake st-image-weston
