53f772d143f0acae1d6d2912f1c9074e8852d907
bcis93 <bryan.cisneros@gmail.com>
2020-04-12 21:42:48 -0600

    updating build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@
 # File - flash.sh
 # Purpose - Used to flash code into STM32MP1 connected through usb
 
-DISTRO=openstlinux-weston MACHINE=stm32mp1 DISTRO_FEATURES_append=" virtualization" source ./layers/meta-st/scripts/envsetup.sh
+DISTRO=openstlinux-weston MACHINE=stm32mp1 source ./layers/meta-st/scripts/envsetup.sh
+
+echo "DISTRO_FEATURES_append=\" virtualization\"" >> ./conf/local.conf
 
 bitbake-layers add-layer ../layers/meta-st/meta-aesd-project
 bitbake-layers add-layer ../layers/meta-openembedded/meta-oe
@@ -14,4 +16,5 @@ bitbake-layers add-layer ../layers/meta-virtualization
 bitbake-layers add-layer ../layers/meta-rust
 bitbake-layers add-layer ../layers/meta-iotedge
 
+set -e
 bitbake st-image-weston
