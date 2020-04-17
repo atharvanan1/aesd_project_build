af0be8ef714b16cc3df7e40c8460c5fd87e332ee
Atharva Nandanwar <atharva.nandanwar@colorado.edu>
2020-04-17 15:54:15 -0600

    new machine added, new machine config added

--- build.sh
+++ build.sh
@@ -1,14 +1,19 @@
 #! /bin/bash
 
 # Name - Atharva Nandanwar
-# File - flash.sh
-# Purpose - Used to flash code into STM32MP1 connected through usb
+# File - build.sh
+# Purpose - used to build for a target machine and distro
 
 git submodule init
 git submodule update --init --recursive
-DISTRO=openstlinux-weston MACHINE=stm32mp1 source ./layers/meta-st/scripts/envsetup.sh
+DISTRO=openstlinux-weston MACHINE=stm32mp1-aesd-project source ./layers/meta-st/scripts/envsetup.sh
 
-echo "DISTRO_FEATURES_append=\" virtualization\"" >> ./conf/local.conf
+if grep -q "virtualization" ./conf/local.conf; then
+    echo -n ""
+else
+    echo "DISTRO_FEATURES_append=\" virtualization\"" >> ./conf/local.conf
+fi
+
 
 bitbake-layers add-layer ../layers/meta-st/meta-aesd-project
 bitbake-layers add-layer ../layers/meta-openembedded/meta-oe
